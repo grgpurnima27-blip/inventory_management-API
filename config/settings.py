0bff872e184e38dd6dffbef8762cc747869f7555
Codersabina199 <adhikarysabu098@gmail.com>
2026-06-29 11:27:45 +0545

    inventory transactions added

--- config/settings.py
+++ config/settings.py
@@ -280,7 +280,7 @@ REST_FRAMEWORK = {
     ),
 
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ),
 
     "DEFAULT_SCHEMA_CLASS":
